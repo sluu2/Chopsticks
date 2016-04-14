e02b802532fb7a0b5e75a96d49e8afb4af0d3f88
sluu2 <sluu2@gmu.edu>
2016-04-14 16:55:15 -0400

    Our Game Project v0.2 alpha
    All the added changes are on the index.html page as well as the document
    that I've emailed to you guys

--- js/win.js
+++ js/win.js
@@ -8,7 +8,6 @@ DA5Game.winState.prototype = {
 	create: function () {
 		startBG = this.add.image(0, 0, 'winSplash');
         this.game.playerHealth = 0;
-		var startPrompt = this.add.bitmapText(this.world.centerX-200, this.world.centerY+180, 'eightbitwonder', 'Press the SPACE key', 24);
         var startKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         startKey.onDown.addOnce(this.startGame, this);
 	},
